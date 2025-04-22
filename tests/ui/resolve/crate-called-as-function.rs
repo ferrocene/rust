e1448c69ca4eff483004463c0fce70126f6e0fa8
Ana Hobden <ana.hobden@ferrous-systems.com>
2025-04-22 08:13:00 -0700

    Fix tests/ui/resolve/crate-called-as-function.rs

--- tests/ui/resolve/crate-called-as-function.rs
+++ tests/ui/resolve/crate-called-as-function.rs
@@ -1,3 +1,9 @@
+//@ revisions: edition2015 edition2018
+//@ [edition2015] edition: 2015
+//@ [edition2018] edition: 2018..
+
 fn main() {
-    ::foo() //~ ERROR cannot find external crate `foo` in the crate root
+    ::foo()
+    //[edition2015]~^ ERROR cannot find external crate `foo` in the crate root
+    //[edition2018]~^^ ERROR cannot find crate `foo` in the list of imported crates
 }
