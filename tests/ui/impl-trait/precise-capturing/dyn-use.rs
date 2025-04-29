8644cb2591d4b055fd91f830a5043403c5234086
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-04-29 11:45:49 -0500

    Fix `dyn-use.rs`

--- tests/ui/impl-trait/precise-capturing/dyn-use.rs
+++ tests/ui/impl-trait/precise-capturing/dyn-use.rs
@@ -1,2 +1,11 @@
+//@ revisions: edition2015 edition2021
+//@[edition2015] edition: 2015
+//@[edition2021] edition: 2021
+//
 fn dyn() -> &'static dyn use<> { &() }
-//~^ ERROR expected one of `!`, `(`, `::`, `<`, `where`, or `{`, found keyword `use`
+//[edition2015]~^ ERROR expected one of `!`, `(`, `::`, `<`, `where`, or `{`, found keyword `use`
+//[edition2021]~^^ ERROR expected identifier
+//[edition2021]~| ERROR precise capturing syntax not allowed
+//[edition2021]~| ERROR at least one trait is required
+
+fn main() {}
