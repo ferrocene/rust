16fcaa40ef015b09726266982b87a8f027510ece
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-04-29 11:20:08 -0500

    Fix `issue-79099.rs`

--- tests/ui/impl-trait/issues/issue-79099.rs
+++ tests/ui/impl-trait/issues/issue-79099.rs
@@ -1,8 +1,12 @@
+//@ revisions: edition2015 edition2021
+//@[edition2015] edition: 2015
+//@[edition2021] edition: 2021
+//
 struct Bug {
     V1: [(); {
         let f: impl core::future::Future<Output = u8> = async { 1 };
         //~^ ERROR `impl Trait` is not allowed in the type of variable bindings
-        //~| ERROR expected identifier
+        //[edition2015]~| ERROR expected identifier
         1
     }],
 }
