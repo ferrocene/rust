47a938a99b01bc6007198813e5395642b6927549
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2025-04-29 17:09:27 +0200

    ui/imports/issue-55457: add revisions to handle variations in help message

--- tests/ui/imports/issue-55457.rs
+++ tests/ui/imports/issue-55457.rs
@@ -1,3 +1,7 @@
+//@ revisions: edition2015 edition2018
+//@ [edition2015] edition: 2015
+//@ [edition2018] edition: 2018..
+
 use NonExistent; //~ ERROR unresolved import `NonExistent`
 use non_existent::non_existent; //~ ERROR unresolved import `non_existent`
 
