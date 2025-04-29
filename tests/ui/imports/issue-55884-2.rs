77406d98d1e2d67f8b1dbea243ae9596094c0dfc
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2025-04-29 17:16:55 +0200

    ui/imports/issue-55884-2: add revisions to handle variations in note messages

--- tests/ui/imports/issue-55884-2.rs
+++ tests/ui/imports/issue-55884-2.rs
@@ -1,9 +1,13 @@
+//@ revisions: edition2015 edition2018
+//@ [edition2015] edition: 2015
+//@ [edition2018] edition: 2018..
+
 mod options {
     pub struct ParseOptions {}
 }
 
 mod parser {
-    pub use options::*;
+    pub use crate::options::*;
     // Private single import shadows public glob import, but arrives too late for initial
     // resolution of `use parser::ParseOptions` because it depends on that resolution itself.
     #[allow(hidden_glob_reexports)]
