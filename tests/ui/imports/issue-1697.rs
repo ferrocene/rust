beb6e4f709bc13fe5ab914670272083d53d2aef4
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-05-05 16:57:34 -0500

    Fix `issue-1697.rs`

--- tests/ui/imports/issue-1697.rs
+++ tests/ui/imports/issue-1697.rs
@@ -1,3 +1,6 @@
+//@ revisions: edition2015 edition2021
+//@[edition2015] edition: 2015
+//@[edition2021] edition: 2021
 // Testing that we don't fail abnormally after hitting the errors
 
 use unresolved::*;
