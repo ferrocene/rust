d7823ce555e5c5eb8a970731a1c1c5dee839759f
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-05-05 16:37:51 -0500

    Fix `import3.rs`

--- tests/ui/imports/import3.rs
+++ tests/ui/imports/import3.rs
@@ -1,3 +1,6 @@
+//@ revisions: edition2015 edition2021
+//@[edition2015] edition: 2015
+//@[edition2021] edition: 2021
 use main::bar; //~ ERROR unresolved import `main`
 
 fn main() { println!("foo"); }
