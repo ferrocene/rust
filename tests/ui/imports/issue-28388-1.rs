8d2cf4fe0d49fb9f0395e012d5585546ef3d814b
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-05-05 20:19:32 -0500

    Fix `issue-28388-1.rs`

--- tests/ui/imports/issue-28388-1.rs
+++ tests/ui/imports/issue-28388-1.rs
@@ -1,3 +1,6 @@
+//@ revisions: edition2015 edition2021
+//@[edition2015] edition: 2015
+//@[edition2021] edition: 2021
 // Prefix in imports with empty braces should be resolved and checked privacy, stability, etc.
 
 use foo::{}; //~ ERROR unresolved import `foo`
