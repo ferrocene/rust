b4368eab374336b98a95e1eb17195316482e1381
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-05-07 16:32:43 -0500

    Fix `issue-17662.rs`

--- tests/ui/issues/auxiliary/issue-17662.rs
+++ tests/ui/issues/auxiliary/issue-17662.rs
@@ -4,8 +4,8 @@ pub trait Foo<'a, T> {
     fn foo(&'a self) -> T;
 }
 
-pub fn foo<'a, T>(x: &'a Foo<'a, T>) -> T {
-    let x: &'a Foo<T> = x;
+pub fn foo<'a, T>(x: &'a dyn Foo<'a, T>) -> T {
+    let x: &'a dyn Foo<T> = x;
     //            ^ the lifetime parameter of Foo is left to be inferred.
     x.foo()
     // ^ encoding this method call in metadata triggers an ICE.
