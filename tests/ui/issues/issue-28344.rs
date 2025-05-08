9511f4f80abf9eba5e7e8647b769c7f590a87ae9
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-05-07 22:33:55 -0500

    Fix `issue-28344.rs`

--- tests/ui/issues/issue-28344.rs
+++ tests/ui/issues/issue-28344.rs
@@ -1,13 +1,18 @@
+//@ revisions: edition2015 edition2021
+//@[edition2015] edition: 2015
+//@[edition2021] edition: 2021
 use std::ops::BitXor;
 
 fn main() {
     let x: u8 = BitXor::bitor(0 as u8, 0 as u8);
-    //~^ ERROR must be specified
-    //~| WARN trait objects without an explicit `dyn` are deprecated
-    //~| WARN this is accepted in the current edition
+    //[edition2015]~^ ERROR must be specified
+    //[edition2015]~| WARN trait objects without an explicit `dyn` are deprecated
+    //[edition2015]~| WARN this is accepted in the current edition
+    //[edition2021]~^^^^ ERROR expected a type, found a trait
 
     let g = BitXor::bitor;
-    //~^ ERROR must be specified
-    //~| WARN trait objects without an explicit `dyn` are deprecated
-    //~| WARN this is accepted in the current edition
+    //[edition2015]~^ ERROR must be specified
+    //[edition2015]~| WARN trait objects without an explicit `dyn` are deprecated
+    //[edition2015]~| WARN this is accepted in the current edition
+    //[edition2021]~^^^^ ERROR expected a type, found a trait
 }
