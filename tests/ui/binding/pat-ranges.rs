e33603a2f5222550221914b4158aa1b749264e01
Lukas Wirth <lukas.wirth@ferrous-systems.com>
2025-05-20 11:37:39 +0200

    Block 1

--- tests/ui/binding/pat-ranges.rs
+++ tests/ui/binding/pat-ranges.rs
@@ -1,7 +1,12 @@
+//@ revisions: edition2015 edition2021
+//@[edition2015]edition:2015..2021
+//@[edition2021]edition:2021..
 //@ run-pass
 // Parsing of range patterns
 
 #![allow(ellipsis_inclusive_range_patterns)]
+
+extern crate self as this;
 
 const NUM1: i32 = 10;
 
@@ -10,11 +15,14 @@ mod m {
 }
 
 fn main() {
-    if let NUM1 ... m::NUM2 = 10 {} else { panic!() }
-    if let ::NUM1 ... ::m::NUM2 = 11 {} else { panic!() }
-    if let -13 ... -10 = 12 { panic!() } else {}
+    #[cfg(edition2015)]
+    {
+        if let NUM1 ... m::NUM2 = 10 {} else { panic!() }
+        if let ::this::NUM1 ... ::this::m::NUM2 = 11 {} else { panic!() }
+        if let -13 ... -10 = 12 { panic!() } else {}
+    };
 
     if let NUM1 ..= m::NUM2 = 10 {} else { panic!() }
-    if let ::NUM1 ..= ::m::NUM2 = 11 {} else { panic!() }
+    if let ::this::NUM1 ..= ::this::m::NUM2 = 11 {} else { panic!() }
     if let -13 ..= -10 = 12 { panic!() } else {}
 }
--- tests/ui/borrowck/trait-impl-argument-difference-ice.rs
+++ tests/ui/borrowck/trait-impl-argument-difference-ice.rs
@@ -1,10 +1,14 @@
+//@ revisions: edition2015 edition2018
+//@[edition2015]edition:2015
+//@[edition2018]edition:2018..
 // Issue https://github.com/rust-lang/rust/issues/123414
 trait MemoryUnit {
     extern "C" fn read_word(&mut self) -> u8;
     extern "C" fn read_dword(Self::Assoc<'_>) -> u16;
-    //~^ WARNING anonymous parameters are deprecated and will be removed in the next edition
-    //~| WARNING this is accepted in the current edition (Rust YYYY) but is a hard error in Rust 2018!
-    //~| ERROR associated type `Assoc` not found for `Self`
+    //[edition2015]~^ WARNING anonymous parameters are deprecated and will be removed in the next edition
+    //[edition2015]~| WARNING this is accepted in the current edition (Rust YYYY) but is a hard error in Rust 2018!
+    //[edition2015]~| ERROR associated type `Assoc` not found for `Self`
+    //[edition2018]~^^^^ ERROR expected one of `!`, `(`, `...`, `..=`, `..`, `::`, `:`, `{`, or `|`, found `<`
 }
 
 struct ROM {}
