6adb43af9ec630229cac3519c6b0dca90d72a546
Ana Hobden <ana.hobden@ferrous-systems.com>
2025-05-20 10:11:56 -0700

    Fix tests/ui/keyword/extern/keyword-extern-as-identifier-use.rs

--- tests/ui/keyword/extern/keyword-extern-as-identifier-use.rs
+++ tests/ui/keyword/extern/keyword-extern-as-identifier-use.rs
@@ -1,3 +1,7 @@
+//@ revisions: edition2015 postedition2015
+//@[edition2015] edition: 2015
+//@[postedition2015] edition: 2018..
+
 use extern::foo; //~ ERROR expected identifier, found keyword `extern`
                  //~| ERROR unresolved import `r#extern`
 
