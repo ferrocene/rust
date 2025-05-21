9e2646d93bf492d279b772ff4f6ff90e16017ed9
Ana Hobden <ana.hobden@ferrous-systems.com>
2025-05-21 09:21:46 -0700

    Fix tests/ui/test-attrs/inaccessible-test-modules.rs

--- tests/ui/test-attrs/inaccessible-test-modules.rs
+++ tests/ui/test-attrs/inaccessible-test-modules.rs
@@ -1,9 +1,12 @@
+//@ revisions: edition2015 postedition2015
+//@[edition2015] edition: 2015
+//@[postedition2015] edition: 2018..
 //@ compile-flags:--test
 
 // the `--test` harness creates modules with these textual names, but
 // they should be inaccessible from normal code.
 use main as x; //~ ERROR unresolved import `main`
-use test as y; //~ ERROR unresolved import `test`
+use test as y; //[edition2015]~ ERROR unresolved import `test`
 
 #[test]
 fn baz() {}
