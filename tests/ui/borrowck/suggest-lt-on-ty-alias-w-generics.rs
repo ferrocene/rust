60f31ad92da7a5e6058f3bfd9515e9625c885f16
Ana Hobden <ana.hobden@ferrous-systems.com>
2025-05-23 10:31:44 -0700

    Fix tests/ui/borrowck/suggest-lt-on-ty-alias-w-generics.rs

--- tests/ui/borrowck/suggest-lt-on-ty-alias-w-generics.rs
+++ tests/ui/borrowck/suggest-lt-on-ty-alias-w-generics.rs
@@ -1,9 +1,13 @@
+//@ revisions: preedition2021 postedition2021
+//@[preedition2021] edition: ..2018
+//@[postedition2021] edition: 2021..
+
 type Lazy<T> = Box<dyn Fn() -> T + 'static>;
 
 fn test(x: &i32) -> Lazy<i32> {
     Box::new(|| {
         //~^ ERROR lifetime may not live long enough
-        //~| ERROR closure may outlive the current function
+        //[preedition2021]~| ERROR closure may outlive the current function
         *x
     })
 }
